edb1515c358a46bddbed975f3ca2ba60f21754e7
MirThomasson <mirthomasson@gmail.com>
2023-02-23 13:26:37 -0500

    free estimate lingo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
           <img src={free} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
-            <span className="text-white">Free</span> Estimates{" "}
-            <span className="text-white">Call Now</span>: 434-386-1898
+            <span className="text-white">Call Now</span> for a{" "}
+            <span className="text-white">Free Estimate</span>: 434-386-1898
           </p>
         </div>
 
